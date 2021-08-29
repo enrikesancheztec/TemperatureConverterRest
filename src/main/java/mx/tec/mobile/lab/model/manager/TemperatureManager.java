621d0f6fe702416d63b9b2cb975d318331dea0ff
Enrique Sanchez <enrike.sanchez@gmail.com>
2021-08-29 14:03:14 -0600

    Added Fix

--- src/main/java/mx/tec/mobile/lab/model/manager/TemperatureManager.java
+++ src/main/java/mx/tec/mobile/lab/model/manager/TemperatureManager.java
@@ -3,8 +3,11 @@ package mx.tec.mobile.lab.model.manager;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import mx.tec.mobile.lab.model.vo.TemperatureConversion;
 
+@Service
 public class TemperatureManager {
 	private List<TemperatureConversion> history;
 	
